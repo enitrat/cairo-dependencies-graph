18cb82560a220e2a59c53705e53847c9898e350d
Krzysztof Szubiczuk <szubiczuk.krzysztof@gmail.com>
2022-10-07 11:13:13 +0200

    Refactored cli to one command only

--- src/cli.py
+++ src/cli.py
@@ -8,19 +8,12 @@ from starkware.cairo.lang.version import __version__
 from logic import generate_graph, print_version
 
 
-@click.group()
 @click.option('--version', is_flag=True, callback=print_version,
               expose_value=False, is_eager=True)
-def cli():
-    pass
-
 @click.command()
 @click.option('--directory', '-d', help='Directory to analyze', required=True)
-def dependencies_graph(directory: str):
+def cli(directory: str):
     sys.exit(generate_graph(directory))
-
-
-cli.add_command(dependencies_graph)
 
 
 def main():

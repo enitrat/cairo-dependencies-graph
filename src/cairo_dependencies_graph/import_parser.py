915ed836f006204a66076cf44608e7ddcb64703b
Krzysztof Szubiczuk <szubiczuk.krzysztof@gmail.com>
2022-10-10 12:07:14 +0200

    Visitor functions back to original version

--- src/cairo_dependencies_graph/import_parser.py
+++ src/cairo_dependencies_graph/import_parser.py
@@ -6,13 +6,15 @@ class ImportParser(Visitor):
 
     def __init__(self):
         super().__init__()
+        self.extracted_imports
 
     def _visit_default(self, obj):
         # top-level code is not generated
         return obj
 
     def visit_CodeBlock(self, elm: CodeBlock):
-        return self.extract_imports(elm)
+        self.extract_imports(elm)
+        return super().visit_CodeBlock(elm)
     
     def extract_imports(self, elm):
         res = []
@@ -22,8 +24,10 @@ class ImportParser(Visitor):
                 path_formatted = path.replace(".", "/") + ".cairo"
                 res.append(path_formatted)
                 res = [*set(res)]
-        return res
+        self.extracted_imports = res
+        return 0
     
     def parse_imports(self, cairo_module):
-        res = self.visit(cairo_module).cairo_file.code_block
+        self.visit(cairo_module)
+        res = self.extracted_imports
         return res
